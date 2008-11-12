55895f90b9ba4903f6fce084d43f22d696dd9560
davcandeia <davcandeia@e8a3b6c2-84f5-11dd-b6e4-1fb4a74862d0>
2008-11-12 17:05:23 +0000

    Removing files

--- trunk/PyFinancialLibrary/PyFinancial/src/allUnitTests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from calcElements.stackTests import calcStackTestCase
-
-if __name__ == "__main__":
-    suite1 = unittest.makeSuite(calcStackTestCase, "test")
-    
-    runner = unittest.TextTestRunner()
-    runner.run(suite1)
