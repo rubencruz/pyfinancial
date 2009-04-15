d18cad4c81aa2cebc45634b115777d8c9184a460
felipelcoutinho <felipelcoutinho@e8a3b6c2-84f5-11dd-b6e4-1fb4a74862d0>
2009-04-15 00:31:31 +0000

    Inserting documentation

--- trunk/PyFinancialLibrary/src/financialLibrary/amortizationTable.py
+++ trunk/PyFinancialLibrary/src/financialLibrary/amortizationTable.py
@@ -1,8 +1,17 @@
 from decimal import Decimal
 
 class AmortizationTable:
+    """ Represents an amortization table.
+    """
+
     
     def __init__(self, numberOfPeriods):
+        """ Constructor for the AmortizationTable class.
+
+        @param numberOfPeriods: amortization periods number
+        @type numberOfPeriods: int
+        """
+        
         self._lines = []
         self.__numberOfPeriods = numberOfPeriods
         zero = Decimal("0")
@@ -13,58 +22,64 @@ class AmortizationTable:
         return self.__numberOfPeriods
     
     def getNumberOfLines(self):
+        """ Return the number of lines. The number of lines is the
+        number of periods plus one.
+
+        @return: numberOfLines
+        @rtype: int
+        """        
         return len(self._lines)
 
-    def getPayment(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getPayment(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col0
      
-    def getInterest(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getInterest(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col1
         
-    def getNewAcumulatedInterest(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getNewAcumulatedInterest(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col2
         
-    def getAmortization(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getAmortization(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col3
 
-    def getNewAcumulatedAmortization(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getNewAcumulatedAmortization(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col4
 
-    def getNewAmountToBePayed(self, n):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
+    def getNewAmountToBePayed(self, lineNumber):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
         return col5
 
-    def setTableLine(self, n, payment, interest, acumulatedInterest, amortization, newAcumulatedAmortization, newAmountToBePayed):
-        self._lines[n] = (payment, interest, acumulatedInterest, amortization, newAcumulatedAmortization, newAmountToBePayed)
+    def setTableLine(self, lineNumber, payment, interest, acumulatedInterest, amortization, newAcumulatedAmortization, newAmountToBePayed):
+        """ Set a specific line of the table.
+        """                
+        self._lines[lineNumber] = (payment, interest, acumulatedInterest, amortization, newAcumulatedAmortization, newAmountToBePayed)
 
-    def setPayment(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (newValue, col1, col2, col3, col4, col5) 
+    def setPayment(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (newValue, col1, col2, col3, col4, col5) 
      
-    def setInterest(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (col0, newValue, col2, col3, col4, col5) 
+    def setInterest(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (col0, newValue, col2, col3, col4, col5) 
         
-    def setNewAcumulatedInterest(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (col0, col1, newValue, col3, col4, col5) 
+    def setNewAcumulatedInterest(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (col0, col1, newValue, col3, col4, col5) 
         
-    def setAmortization(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (col0, col1, col2, newValue, col4, col5) 
+    def setAmortization(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (col0, col1, col2, newValue, col4, col5) 
 
-    def setNewAcumulatedAmortization(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (col0, col1, col2, col3, newValue, col5) 
+    def setNewAcumulatedAmortization(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (col0, col1, col2, col3, newValue, col5) 
 
-    def setNewAmountToBePayed(self, n, newValue):
-        col0, col1, col2, col3, col4, col5 = self._lines[n]
-        self._lines[n] = (col0, col1, col2, col3, col4, newValue)
-        
-        
+    def setNewAmountToBePayed(self, lineNumber, newValue):
+        col0, col1, col2, col3, col4, col5 = self._lines[lineNumber]
+        self._lines[lineNumber] = (col0, col1, col2, col3, col4, newValue)
          
